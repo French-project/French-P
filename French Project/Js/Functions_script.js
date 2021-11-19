1270b04d817cc75b71d8b054353e34d425b2a2ba
French-project <93148038+French-project@users.noreply.github.com>
2021-11-19 20:10:34 +0100

    Add files via upload

---
+++ French Project/Js/Functions_script.js
@@ -0,0 +1,6 @@
+let Func_1 = function () {
+    addEventListener("click", function () {
+        document.getElementById("mybtn");
+        window.scrollTo({top : 0, behavior : "smooth"})
+    })
+    }
